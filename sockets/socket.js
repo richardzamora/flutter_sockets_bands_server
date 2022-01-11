b543a6f0b6c7bfe432edd2e84bc72fa76440efee
Richard Zamora <richardjavierzamora@gmail.com>
2022-01-11 09:49:49 -0500

    Fin sección 3

---
+++ sockets/socket.js
@@ -0,0 +1,13 @@
+const {io} = require('../index');
+
+
+io.on('connection', client => {
+    console.log('Client connected');
+
+    client.on('disconnect', () => { console.log('Client disconnected') });
+
+    client.on('mensaje', (payload) => {
+        console.log('Mensaje', payload);
+        io.emit('mensaje', {admin: "Nuevo Mensaje"})
+    })
+  });
